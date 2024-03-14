f971eca45c78685f774d5cd06ab6c0f5b84825d3
Michael Wong <42653786+micompany4@users.noreply.github.com>
2024-03-14 16:11:35 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n49r!h)4v5x)9i=kixnma6mc9-y=z(k&g^oing%dw8n4tly6!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
